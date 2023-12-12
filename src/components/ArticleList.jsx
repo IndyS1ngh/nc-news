5491654b003c1f4d4e365adbda2b917a86945869
IndyS1ngh <inderpreetsingh@hotmail.co.uk>
2023-12-12 12:26:49 +0000

    refactor code based on feedback and work on additional styling

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -4,24 +4,37 @@ import ArticleItem from "./ArticleItem";
 
 const ArticleList = () => {
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     getArticles()
       .then((apiArticles) => {
         setArticles(apiArticles);
+        setIsLoading(false);
       })
-      .catch((err) => {
-        console.log(err);
+      .catch(() => {
+        setIsError(true);
+        setIsLoading(false);
       });
   }, []);
 
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>Something went wrong...</h2>;
+  }
+
   return (
-    <ul className="article-list">
+    <div>
       <button className="filter-by-topic">Filter by Topic</button>
-      {articles.map((article) => {
-        return <ArticleItem article={article} key={article.article_id} />;
-      })}
-    </ul>
+      <ul className="article-list">
+        {articles.map((article) => {
+          return <ArticleItem article={article} key={article.article_id} />;
+        })}
+      </ul>
+    </div>
   );
 };
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-
 const NavBar = () => {
   return (
     <nav className="navbar">
